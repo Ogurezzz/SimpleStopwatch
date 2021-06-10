ed2d2d5549e6502517ab83beda3a2e58f75e313b
Ogurezzz <gurevoleg@gmail.com>
2021-06-10 23:29:55 +0300

    Changed configuration

--- Firmware/main.c
+++ Firmware/main.c
@@ -11,30 +11,36 @@
 
 volatile uint32_t secondsPassed = 0;
 
-#define BUILIN_LED_DDR				DDRB
+/*#define BUILIN_LED_DDR				DDRB
 #define BUILIN_LED_PORT				PORTB
-#define BUILIN_LED_PIN				PORTB5
+#define BUILIN_LED_PIN				PORTB5 */
 
 #define BTN_DDR						DDRC
 #define BTN_PORT					PORTC
-#define  BTN_RESET 					PORTC4
+#define BTN_RESET 					PORTC0
 
-#define TIMER_START 				TCCR1B |= _BV(CS12)|_BV(CS10) 				//Включаем таймер с предделителем 1024 
+#define TIMER_START 				TCCR1B |= _BV(CS12) 				//Включаем таймер с предделителем 256 
 #define TIMER_STOP 					TCCR1B &= ~(_BV(CS12)|_BV(CS10));TCNT1 = 0  //Останавливаем и сбрасываем таймер
 
 ISR(TIMER1_COMPA_vect) 				//Прерывание. 1 раз в секунду.
 {
-	BUILIN_LED_PORT ^= _BV(BUILIN_LED_PIN);
+	//BUILIN_LED_PORT ^= _BV(BUILIN_LED_PIN);
 	secondsPassed++;
 	setTimeToPrint(secondsPassed);
 }
 
-/*ISR(TIMER2_COMPA_vect) 			//Прерывание. 1 раз в мс. Оставлю на будущее, мало ли пригодится где :)
+ISR(TIMER2_COMPA_vect) 			//Прерывание. 1 раз в мс. Оставлю на будущее, мало ли пригодится где :)
 {
-	
-}*/
+	printDigit();
+	/*static uint8_t ticks = 0;
+	if (ticks>10){
+		
+		ticks=0;
+	}
+	ticks++;*/
+}
 
-ISR(PCINT1_vect){					//Прерывание по изменению пинов CPINT14..8
+ISR(PCINT1_vect){					//Прерывание по изменению пинов PCINT14..8
 	if (bit_is_clear(PINC, BTN_RESET))
 	{
 		TIMER_STOP;
@@ -60,30 +66,32 @@ int main(void)
 	BTN_PORT |= _BV(BTN_RESET); 	//Включаем подтяжку на пинах для кнопок
 
 	//Настройка вывода на индикаторный светодиод.
-	BUILIN_LED_DDR |= _BV(BUILIN_LED_PIN); 		//Включаем выход на индикаторный диод
+	//BUILIN_LED_DDR |= _BV(BUILIN_LED_PIN); 		//Включаем выход на индикаторный диод
 
 	//Настраиваем таймер часов (1с)
-	DDRB = _BV(PINB1);
-	TCCR1A = _BV(COM0A0); 						// Установить режим Compare Match + вывод ШИМ на ногу OC1A (PB1)
-	TCCR1B = _BV(WGM12)|_BV(CS12);//|_BV(CS10); 	// Сброс по достижению заданног значения в OCR1A. Предделитель 1024.
-	//OCR1A = 15645; 								// Установить значение в регистр сравнения. Значение для китайского кварца. Подбирать опытным путем.
-	//OCR1A = 15625;							// Значение для нормального кварца
-	OCR1A = 15625*2;
+	//DDRB = _BV(PINB1);
+	//TCCR1A = _BV(COM0A0); 					// Установить режим Compare Match + вывод ШИМ на ногу OC1A (PB1)
+	TCCR1B = _BV(WGM12);			 			// Сброс по достижению заданног значения в OCR1A. Предделитель 256.
+	TIMER_START;
+	//OCR1A = 15645; 							// Установить значение в регистр сравнения. Значение для китайского кварца. Подбирать опытным путем.
+	//OCR1A = 15625;							
+	OCR1A = 15625*2;							// Значение для нормального кварца 8MHz
 	TCNT1 = 0;									// Установить начальное значение счётчиков
 	TIMSK1 |= _BV(OCIE1A);						// Разрешаем прерывание По совпадению с OCR1A
 
 	//Настраиваем таймер тиков (1мс)
-	/*TCCR2A = 0;
+	TCCR2A = 0;
 	TCCR2A = _BV(WGM21);						// Установить режим Compare Match
-	TCCR2B = _BV(CS22)|_BV(CS20);				// Предделитель 64
+	TCCR2B = _BV(CS22);							// Предделитель 64
+	//TCCR2B = _BV(CS22)|_BV(CS21)|_BV(CS20);		//Предделитель 1024
 	OCR2A = 125; 								// Установить значение в регистр сравнения
 	TCNT2 = 0;									// Установить начальное значение счётчиков
-	TIMSK2 |= _BV(OCIE2A);						// Разрешаем прерывание По совпадению с OCR2A*/
+	TIMSK2 |= _BV(OCIE2A);						// Разрешаем прерывание По совпадению с OCR2A
 	
 	// Включаем прерывания
 	asm volatile("sei");
 	while (1) 
 	{
-		printDigit();
+		
 	}
 }
